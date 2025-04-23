e910ce9212eb8f41f2e1382b116ebb7973d21e61
Mahdie <152741984+mahdiepirzade@users.noreply.github.com>
2025-04-23 15:19:14 +0330

    Update 12.py

--- 12.py
+++ 12.py
@@ -1,11 +1,13 @@
 
-#2
+num = int(input("plz enter a num: "))
+
 copy_num = num
 original_num = num
 
+temp = num
 count = 0
-while num > 0:
-    num = num // 10
+while temp > 0:
+    temp = temp // 10
     count = count + 1
 
 count = count - 1
@@ -16,6 +18,9 @@ while count >= 0:
     reversed_ = reversed_ + rem * (10 ** count)
     count = count - 1
 
+
+
+print("Reveresd num is: ", reversed_)
 if original_num == reversed_:
     print("they are palindrome. ")
 else:
